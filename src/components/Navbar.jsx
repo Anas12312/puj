b7bf2cd7cf24605bba7e01bc77ec669171e56500
Zyad Abdul-Nasser <zizo_007@live.com>
2023-12-28 00:13:01 +0200

    button gradient

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Navbar({
   scrollToMainRef,
-  scrollToAboutRef, 
+  scrollToAboutRef,
   scrollToTokenomicsRef,
   scrollToRoadmapRef,
 }) {
@@ -17,15 +17,15 @@ export default function Navbar({
   }
   return (
     <div className='nav-bar z-10 '>
-        <div className='mx-5'><img src="https://station.jup.ag/img/jupiter-logo.svg" alt="" /></div>
-        <div className='text-lg font-bold mr-16 text-white'>Puj Station</div>
-        <div className='nav-bar-item trans' onMouseEnter={mouseEnter} onMouseLeave={mouseLeave} onClick={scrollToMainRef}>Home</div>
-        <div className='nav-bar-item trans' onMouseEnter={mouseEnter} onMouseLeave={mouseLeave} onClick={scrollToAboutRef}>About</div>
-        <div className='nav-bar-item trans' onMouseEnter={mouseEnter} onMouseLeave={mouseLeave} onClick={scrollToTokenomicsRef}>Tokenomics</div>
-        <div className='nav-bar-item trans' onMouseEnter={mouseEnter} onMouseLeave={mouseLeave} onClick={scrollToRoadmapRef}>Roadmap</div>
-        <div className='nav-bar-item trans' onMouseEnter={mouseEnter} onMouseLeave={mouseLeave} onClick={comingSoon}>Air Drop</div>
-        <div className='nav-bar-item trans mr-5' onMouseEnter={mouseEnter} onMouseLeave={mouseLeave} onClick={comingSoon}>Pre Sale</div>
-        <div className='nav-bar-buy-now trans' onClick={comingSoon}>buy now</div>
-    </div>
+      <div className='mx-5'><img src="https://station.jup.ag/img/jupiter-logo.svg" alt="" /></div>
+      <div className='text-lg font-bold mr-16 text-white'>Puj Station</div>
+      <div className='nav-bar-item trans' onMouseEnter={mouseEnter} onMouseLeave={mouseLeave} onClick={scrollToMainRef}>Home</div>
+      <div className='nav-bar-item trans' onMouseEnter={mouseEnter} onMouseLeave={mouseLeave} onClick={scrollToAboutRef}>About</div>
+      <div className='nav-bar-item trans' onMouseEnter={mouseEnter} onMouseLeave={mouseLeave} onClick={scrollToTokenomicsRef}>Tokenomics</div>
+      <div className='nav-bar-item trans' onMouseEnter={mouseEnter} onMouseLeave={mouseLeave} onClick={scrollToRoadmapRef}>Roadmap</div>
+      <div className='nav-bar-item trans' onMouseEnter={mouseEnter} onMouseLeave={mouseLeave} onClick={comingSoon}>Air Drop</div>
+      <div className='nav-bar-item trans mr-5' onMouseEnter={mouseEnter} onMouseLeave={mouseLeave} onClick={comingSoon}>Pre Sale</div>
+      <div className='nav-bar-buy-now trans z-10' onClick={comingSoon}>Buy Now</div>
+      <div className='absolute right-6 bg-[#121D28] rounded-xl  w-28 h-11 z-0'></div></div>
   )
 }
