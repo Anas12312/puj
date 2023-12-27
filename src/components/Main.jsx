06e8d1c1ba8e22acd848c617cb27d59efdc617e3
anas12312 <anashesham000@gmail.com>
2023-12-28 00:12:33 +0200

    anas

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,18 +1,17 @@
 import React, { useRef } from 'react'
-import background from './../../public/space.png'
 import Navbar from './Navbar'
+import Home from './Home'
 
 export default function Main() {
 
 
     return (
-        <div className='w-full h-full relative flex justify-start items-start'
-          style={{backgroundImage: `url(${background})`, backgroundRepeat:'no-repeat', backgroundSize:'100%'}}
+        <div className='w-full h-full relative flex flex-col justify-between items-center'
+          style={{backgroundImage: `url(./../../space.png)`, backgroundRepeat:'no-repeat', backgroundSize:'100%'}}
         >
           <Navbar />
-          {/* <div className='absolute left-[10%] text-white'>
-            aa
-          </div> */}
+          <Home />
+
 
         </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,13 +18,13 @@ export default function Navbar({
   return (
     <div className='nav-bar z-10 '>
         <div className='mx-5'><img src="https://station.jup.ag/img/jupiter-logo.svg" alt="" /></div>
-        <div className='text-lg font-bold mr-16 text-white'>Puj Station</div>
-        <div className='nav-bar-item trans' onMouseEnter={mouseEnter} onMouseLeave={mouseLeave} onClick={scrollToMainRef}>Home</div>
-        <div className='nav-bar-item trans' onMouseEnter={mouseEnter} onMouseLeave={mouseLeave} onClick={scrollToAboutRef}>About</div>
-        <div className='nav-bar-item trans' onMouseEnter={mouseEnter} onMouseLeave={mouseLeave} onClick={scrollToTokenomicsRef}>Tokenomics</div>
-        <div className='nav-bar-item trans' onMouseEnter={mouseEnter} onMouseLeave={mouseLeave} onClick={scrollToRoadmapRef}>Roadmap</div>
-        <div className='nav-bar-item trans' onMouseEnter={mouseEnter} onMouseLeave={mouseLeave} onClick={comingSoon}>Air Drop</div>
-        <div className='nav-bar-item trans mr-5' onMouseEnter={mouseEnter} onMouseLeave={mouseLeave} onClick={comingSoon}>Pre Sale</div>
+        <div className='text-lg font-bold mr-16 text-white font-mono'>Puj Station</div>
+        <div className='nav-bar-item' onMouseEnter={mouseEnter} onMouseLeave={mouseLeave} onClick={scrollToMainRef}>Home</div>
+        <div className='nav-bar-item' onMouseEnter={mouseEnter} onMouseLeave={mouseLeave} onClick={scrollToAboutRef}>About</div>
+        <div className='nav-bar-item' onMouseEnter={mouseEnter} onMouseLeave={mouseLeave} onClick={scrollToTokenomicsRef}>Tokenomics</div>
+        <div className='nav-bar-item' onMouseEnter={mouseEnter} onMouseLeave={mouseLeave} onClick={scrollToRoadmapRef}>Roadmap</div>
+        <div className='nav-bar-item' onMouseEnter={mouseEnter} onMouseLeave={mouseLeave} onClick={comingSoon}>Air Drop</div>
+        <div className='nav-bar-item mr-5' onMouseEnter={mouseEnter} onMouseLeave={mouseLeave} onClick={comingSoon}>Pre Sale</div>
         <div className='nav-bar-buy-now trans' onClick={comingSoon}>buy now</div>
     </div>
   )
